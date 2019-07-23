63ab69e668f8859b90023eb3e0aca08cfc9dcf95
huangzx <1046934538@qq.com>
2019-07-23 15:36:46 +0800

    调整目录结构,修改build.sh,在en中添加测试文件

--- wiki/build.sh
+++ wiki/build.sh
@@ -1,22 +1,55 @@
 #!/bin/bash
 
 if [ ! -n "$1" ]; then
-echo Lack of parameters. 
-exit 1
+build_target="usage"
+else
+build_target=$1
 fi
 
 TOP_DIR=$(pwd)
-if [ -e "${TOP_DIR}/json/${1}.json" ]; then
-json="${TOP_DIR}/json/${1}.json"
+
+function usage(){
+    echo "----------USAGE: build.sh modules-----------"
+    echo "| default                   show this menu |"
+    echo "| clean                clean all .md files |"
+    echo "| board name               build .md files |"
+    echo "--------------------------------------------"
+}
+
+function build_clean(){
+    echo clean *.md in zh_CN
+    cd ${TOP_DIR}/zh_CN/base && rm *.md && cd -
+    echo clean *.md in en
+    cd ${TOP_DIR}/en && rm *.md && cd -
+}
+
+function build_md(){
+    cd ${TOP_DIR}/zh_CN/base
+    files=$(ls *.mdpp)
+    for file in $files
+    do 
+        markdown-pp $file -f $json -o ${file%.*}.md
+    done
+
+    cd ${TOP_DIR}/en
+    files=$(ls *.mdpp)
+    for file in $files
+    do 
+        markdown-pp $file -f $json -o ${file%.*}.md
+    done   
+}
+
+if [ $build_target == usage ]; then
+usage
+exit 0
+elif [ $build_target == clean ]; then
+build_clean
+exit 0
+elif [ -e "${TOP_DIR}/json/${build_target}.json" ]; then
+json="${TOP_DIR}/json/${build_target}.json"
+build_md
+exit 0
 else
-echo No find ${1}.json in json file
+echo No such command or no ${build_target}.json in json file
 exit 1
 fi
-
-cd ${TOP_DIR}/zh_CN/base
-
-files=$(basename `ls *.mdpp`)
-for file in $files
-do
-    markdown-pp $file.mdpp -f $json -o ${file%*.}.md
-done
